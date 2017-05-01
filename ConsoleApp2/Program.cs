871029b82d7131b08c92b4c90c263e5ee4d9d1d9
LisaAndersen <asil134@gmail.com>
2017-05-01 15:33:38 +0200

    test2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Indtast navn");
+            string navn = Console.ReadLine();
+            Console.WriteLine("Welcome {0}", navn.ToUpper());
+            MakeDuck();
+            Console.WriteLine("tryk tast for at fortsætte!");
+            Console.ReadKey();
+        }
+        static void MakeDuck()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("=");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("(");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("o");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" )");
+            Console.WriteLine(" (  > )/");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("^^^^^^^^^^^^");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
