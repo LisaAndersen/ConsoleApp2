05526ddfd876ca7c2c76b9480a3c4501720a9ad8
LisaAndersen <asil134@gmail.com>
2017-05-02 16:00:37 +0200

    Commit2 dag 2

---
+++ 10Tabel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10Tabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int j = 1; j < 11; j++)
+                {
+                    if (i * j > 50)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    string tal = (i * j).ToString();
+                    Console.Write(tal.PadLeft(4));
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ArbejdMedStrenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbejdMedStrenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string enavn = "Cronberg";
+
+            string samletStreng =$"{fornavn} {enavn}";
+            Console.WriteLine(samletStreng);
+            Console.WriteLine(samletStreng.ToUpper());
+            Console.WriteLine(samletStreng.ToLower());
+            Console.WriteLine(samletStreng.Substring(7, 4));
+
+            string[] s = samletStreng.Split(' ');
+
+            Console.WriteLine(s[1]);
+            Console.ReadKey();
+        }
+    }
+}
